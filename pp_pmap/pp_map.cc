50c1501505e6a44c5dbf8ec8d29f42d280da6872
Aaron McQuade <am047613@ohio.edu>
2017-09-19 08:54:52 -0400

    hella changes

--- pp_pmap/pp_map.cc
+++ pp_pmap/pp_map.cc
@@ -11,10 +11,10 @@
 
 int main(int argc, char** argv) {
 	// input handling
-	if(argc != 2){
+	if (argc != 2) {
 		printf("Error: Enter process number\n");
 		exit(-1);
-	} else if(argc > 2){
+	} else if (argc > 2) {
 		printf("Error: Too many args\n");
 		exit(-1);
 	} else {
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
 		int long p, pid;
 
 		p = strtol(argv[1], NULL, 10);
-		if(p == 0){
+		if (p == 0) {
 			printf("Error: Input was not a number\n");
 			exit(-1);
 		}
@@ -35,26 +35,32 @@ int main(int argc, char** argv) {
 			while (ep = readdir (dp)) {
 				pid = strtol(ep->d_name, NULL, 10);
 				// match pid and p
-				if( ( ep->d_type == DT_DIR ) && ( pid > 0) && (p == pid)) {
+				if ( ( ep->d_type == DT_DIR ) && ( pid > 0) && (p == pid)) {
 					// open specified proc file
-					char path[50];
+					//char path[50];
+					char buffer[25];
 					DIR *proc_dir;
-					strcpy(path, "/proc/");
-					strcat(path, argv[1]);
-					printf("%s\n", path);
-					proc_dir = opendir(path);
-					if(proc_dir == NULL){
+					int n;
+					n = sprintf(buffer, "/proc/%lu/maps", pid);
+
+					//strcpy(path2, argv[1]);
+					//strcpy(path, "/proc/");
+					//strcat(path, argv[1]);
+					printf("%s\n", buffer);
+
+					proc_dir = opendir(buffer);
+
+					if (proc_dir == NULL) {
 						printf("Error: Process could not be accessed\n");
 						closedir(dp);
 						closedir(proc_dir);
 						exit(-1);
 					} else {
 						// read from maps file
-						strcat(path, "maps");
 						FILE *fptr;
 						char ch;
-						fptr = fopen(path, "r");
-						if(fptr == NULL){
+						fptr = fopen(buffer, "r");
+						if (fptr == NULL) {
 							printf("Error: Could not read maps\n");
 							closedir(dp);
 							closedir(proc_dir);
--- pp_pmap/pp_map.cpp
+++ pp_pmap/pp_map.cpp
@@ -1,7 +1,7 @@
 // Aaron McQuade
 // CS 4420
 // Assignment 1 - Part 1
-// pp_map.cc
+// pp_map.cpp - C++ implementation
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,15 +13,18 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 #include <sstream>
 
 using namespace std;
+
+void print_data(vector<string> d);
 int main(int argc, char** argv) {
-	// input handling
-	if(argc != 2){
+	// check number of args
+	if (argc != 2) {
 		printf("Error: Enter process number\n");
 		exit(-1);
-	} else if(argc > 2){
+	} else if (argc > 2) {
 		printf("Error: Too many args\n");
 		exit(-1);
 	} else {
@@ -30,51 +33,88 @@ int main(int argc, char** argv) {
 		int long p, pid;
 
 		p = strtol(argv[1], NULL, 10);
-		if(p == 0){
+		if (p == 0) {
 			printf("Error: Input was not a number\n");
 			exit(-1);
 		}
 		// printf("%lu\n", p);
 		dp = opendir ("/proc");
 		if (dp != NULL) {
-			// read processes
 			printf("Start\tEnd\tSize\tPerm\tPath\n");
+
+			// read processes
 			while (ep = readdir (dp)) {
 				pid = strtol(ep->d_name, NULL, 10);
-				// match pid and p
-				if( ( ep->d_type == DT_DIR ) && ( pid > 0) && (p == pid)) {
+				// match pid and p && dir type
+				if ( ( ep->d_type == DT_DIR ) && ( pid > 0) && (p == pid)) {
 					// open specified proc file
 					char path[50];
 					DIR *proc_dir;
 					strcpy(path, "/proc/");
 					strcat(path, argv[1]);
 					printf("%s\n", path);
+
+					// open proc dir and check
 					proc_dir = opendir(path);
-					if(proc_dir == NULL){
+					if (proc_dir == NULL) {
 						printf("Error: Process could not be accessed\n");
 						closedir(dp);
 						closedir(proc_dir);
 						exit(-1);
 					} else {
 						// read from maps file
-						strcat(path, "maps");
+						// add maps to path param
+						// strcat(path, "maps");
 
+						// declare stream + string vars
 						ifstream inp;
-                  string line, pp, path2;
-                  vector<string> info;
+						string line, pp, path2;
+						vector<string> numbers;
 
-                  pp = argv[1];
-                  path2 = "/proc/" + pp + "/maps";
-                  inp.open(path2.c_str());
-                  getline(inp, line);
-                  cout << line << endl;
+						// set path and open stream to file
+						pp = argv[1];
+						path2 = "/proc/" + pp + "/maps";
+						inp.open(path2.c_str());
+						getline(inp, line);
 
-                  while( !inp.eof() ){
-                     cout << line;
-                     getline(inp, line);
-                  }
+						while ( !inp.eof() ) {
+							// read line and tokenize
+							stringstream ss(line);
+							string s;
+							vector<string> data;
+							int i = 0;
+							while ( !ss.eof() ) {
+								ss >> s;
+								if (i == 0 || i == 1 || (i == 5 && s != "0")) {
+									if (i == 0 ) {
+										string start, end;
+										size_t found = s.find('-');
+										if (found) {
+											start = s.substr(0, found);
+											//cout << start << endl;
+											end = s.substr(found + 1, s.length() - found);
+											//cout << end << endl;
+											data.push_back(start);
+											data.push_back(end);
+										} else {
+										exit(-1);
+										// error looking for address
+										// implement later
+										}
+									} else {
+										data.push_back(s);
+									}
+								}
 
-                  inp.close();
+								++i;
+							}
+							print_data(data);
+							cout << endl;
+							getline(inp, line);
+
+						}
+
+						inp.close();
 					}
 				}
 			}
@@ -86,4 +126,11 @@ int main(int argc, char** argv) {
 		}
 	}
 	return 0;
+}
+
+// print formatted data
+void print_data(vector<string> d) {
+	for (int x = 0; x < d.size(); ++x) {
+		cout << d[x] << ' ';
+	}
 }
